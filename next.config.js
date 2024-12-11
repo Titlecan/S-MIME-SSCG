2b5fabae1de6c8b6661822b81a78be2297c6dbee
Titlecan <a@titlecan.cn>
2024-12-11 15:37:44 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export', // 启用静态导出
+  images: {
+    unoptimized: true // 支持静态导出
+  },
+  
+  // 国际化配置
+  i18n: {
+    locales: ['zh', 'en'],
+    defaultLocale: 'zh'
+  }
 }
 
 module.exports = nextConfig
